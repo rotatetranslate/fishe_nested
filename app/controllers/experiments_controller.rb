daa0965f2ac11bcf1bfb8984912d1410a9da1ad9
Adam Berro <adamberro@gmail.com>
2016-06-01 21:08:31 -0700

    add nested form

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -17,8 +17,38 @@ class ExperimentsController < ApplicationController
     @log = @experiment.logs.new
   end
 
+  def create
+    @scientist = Scientist.find(params[:scientist_id])
+    @experiment = @scientist.experiments.new(experiment_params)
+    if @experiment.save
+      redirect_to(
+        scientist_path(params[:scientist_id]),
+        notice: "Experiment and its first log were created."
+      )
+    else
+      render 'new'
+    end
+  end
+
   def edit
     @experiment = Experiment.find(params[:id])
+  end
+
+  private
+
+  def experiment_params
+    params.require(:experiment).permit(
+      :name,
+      :summary,
+      :budget,
+      log_attributes: [
+        :subject_name,
+        :weight,
+        :treatments,
+        :deceased,
+        :comment
+      ]
+    )
   end
 
 end
